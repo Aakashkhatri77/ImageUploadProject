aaf0c816b3c8847f75946f19695a92c559d401db
Aakashkhatri77 <aakash.kc.77@gmail.com>
2022-01-03 09:54:53 +0545

    image validation

--- ImageUploadProject/Controllers/StudentController.cs
+++ ImageUploadProject/Controllers/StudentController.cs
@@ -15,7 +15,7 @@ namespace ImageUploadProject.Controllers
         public StudentController(ApplicationDbContext _db, IWebHostEnvironment hostEnvironment)
         {
             this.db = _db;
-            this._hostEnvironment = hostEnvironment;   
+            this._hostEnvironment = hostEnvironment;
         }
 
         //public StudentController(IWebHostEnvironment hostEnvironment)
@@ -37,33 +37,65 @@ namespace ImageUploadProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,ImageFile")] Student student)
         {
-
-            // Save image to wwwroot/image
-            string wwwRootPath = _hostEnvironment.WebRootPath;
-            string fileName = Path.GetFileNameWithoutExtension(student.ImageFile.FileName);
-            string extension = Path.GetExtension(student.ImageFile.FileName);
-            student.Profile = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-            string path = Path.Combine(wwwRootPath + "/Image/", fileName);
-            using (var fileStream = new FileStream(path, FileMode.Create))
+            if (ModelState.IsValid == true)
             {
-                await student.ImageFile.CopyToAsync(fileStream);
-            }
+                // Save image to wwwroot/image
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(student.ImageFile.FileName);
+                string extension = Path.GetExtension(student.ImageFile.FileName);
+                IFormFile postedFile = student.ImageFile;
+                long length = postedFile.Length;
+                if (extension.ToLower() == ".jpg" || extension.ToLower() == ".png" || extension.ToLower() == ".jpeg")
+                {
+                    if (length <= 1000000)
+                    {
+                        student.Profile = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                        string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+                        using (var fileStream = new FileStream(path, FileMode.Create))
+                        {
+                            await student.ImageFile.CopyToAsync(fileStream);
+                        }
+                        //Insert Record
+                        db.Students.Add(student);
+                        int Student = await db.SaveChangesAsync();
+                        if (Student > 0)
+                        {
+                            TempData["CreateMessage"] = "<script>alert('Data Inserted Successfully.')</script>";
+                            ModelState.Clear();
+                            return RedirectToAction(nameof(Index));
+                        }
+                        else
+                        {
+                            TempData["CreateMessage"] = "<script>alert('Data Not Inserted.')</script>";
+                        }
+                    }
+                    else
+                    {
+                        TempData["SizeMessage"] = "<script>alert('Image file should be less than 1mb')</script>";
 
-            //Insert Record
-            db.Students.Add(student);
-            await db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+                    }
+                }
+                else
+                {
+                    TempData["ExtentionMessage"] = "<script>alert('Format Not Supported')</script>";
+                }
+
+            }
+            /*            return RedirectToAction(nameof(Index));
+            */
             return View(student);
+
         }
 
         //Get: Edit
         public async Task<IActionResult> Edit(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return NotFound();
             }
             var student = await db.Students.FindAsync(id);
+            TempData["imgPath"] = student.Profile;
             if (student == null)
             {
                 return NotFound();
@@ -72,24 +104,82 @@ namespace ImageUploadProject.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(Student student)
+        public async Task<IActionResult> Edit(IFormFile file, Student student)
         {
-
-            string wwwRootPath = _hostEnvironment.WebRootPath;
-            string fileName = Path.GetFileNameWithoutExtension(student.ImageFile.FileName);
-            string extension = Path.GetExtension(student.ImageFile.FileName);
-            student.Profile = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
-            string path = Path.Combine(wwwRootPath + "/Image/", fileName);
-            using (var fileStream = new FileStream(path, FileMode.Create))
+            if (ModelState.IsValid == true)
             {
-                await student.ImageFile.CopyToAsync(fileStream);
+                if (student.ImageFile != null)
+                {
+                // Update image to wwwroot/image
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(student.ImageFile.FileName);
+                string extension = Path.GetExtension(student.ImageFile.FileName);
+                IFormFile postedFile = student.ImageFile;
+                long length = postedFile.Length;
+                if (extension.ToLower() == ".jpg" || extension.ToLower() == ".png" || extension.ToLower() == ".jpeg")
+                {
+                    if (length <= 1000000)
+                    {
+                        student.Profile = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                        string path = Path.Combine(wwwRootPath + "/Image/" + fileName);
+                            using (var fileStream = new FileStream(path, FileMode.Create))
+                            {
+                                await student.ImageFile.CopyToAsync(fileStream);
+                            }
+                            db.Entry(student).State = EntityState.Modified;
+                            int Student = await db.SaveChangesAsync();
+                        if (Student > 0)
+                        {
+                                
+                                string oldImgPath = (TempData["imgPath"]).ToString();
+/*                                string ImagePath = Path.Combine(wwwRootPath, "/Image/", fileName);
+*/                                if (System.IO.File.Exists(oldImgPath))
+                            {
+                                System.IO.File.Delete(oldImgPath);
+                            }
+                            TempData["UpdateMessage"] = "<script>alert('Data Updated Successfully.')</script>";
+                            ModelState.Clear();
+                            return RedirectToAction("Index", "Student");
+                        }
+                        else
+                        {
+                            TempData["UpdateMessage"] = "<script>alert('Data Not Updated.')</script>";
+                        }
+                    }
+                    else
+                    {
+                        TempData["SizeMessage"] = "<script>alert('Image Size should be less than 1 MB')</script>";
+                    }
+                }
+                else
+                {
+                    TempData["ExtensionMessage"] = "<script>alert('Format Not Supported')</script>";
+                }
             }
-            //db.Entry(student).State = EntityState.Modified;
-            db.Update(student);
-            await db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+                else
+                {
+                    student.Profile = TempData["imgPath"].ToString();
+                    db.Entry(student).State = EntityState.Modified;
+                    int Student = await db.SaveChangesAsync();
+                    if (Student > 0)
+                    {
+                        TempData["UpdateMessage"] = "<script>alert('Data Updated Successfully.')</script>";
+                        ModelState.Clear();
+                        return RedirectToAction("Index", "Student");
+                    }
+                    else
+                    {
+                        TempData["UpdateMessage"] = "<script>alert('Data Not Updated.')</script>";
+                    }
 
+                }
+                
+            }
+            return View();
         }
+    
+      
+
 
         //Get: Details
         public IActionResult Details(int id)
@@ -116,6 +206,8 @@ namespace ImageUploadProject.Controllers
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
         {
+            
+
             var student = await db.Students.FindAsync(id);
             db.Students.Remove(student);
             await db.SaveChangesAsync();
